933ea4dae4a4812c9016b4a14b14627836feff4d
SethiosXplore <pro.sethiosxplore@gmail.com>
2024-01-09 22:00:55 +0200

    Final config and fix

---
+++ app/Models/Shop/Order.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models\Shop;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = 'shop_orders';
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'number',
+        'total_price',
+        'status',
+        'currency',
+        'shipping_price',
+        'shipping_method',
+        'notes',
+    ];
+
+    protected $casts = [
+        'status' => OrderStatus::class,
+    ];
+
+    public function address(): MorphOne
+    {
+        return $this->morphOne(OrderAddress::class, 'addressable');
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'shop_customer_id');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class, 'shop_order_id');
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
